a2c10222e93a913d039de29228c8a91eb5b7ef1e
Konisz <kmateusz2012@gmail.com>
2021-05-17 23:15:59 +0200

    get values from db and display

--- connect.php
+++ connect.php
@@ -5,18 +5,4 @@ $password = "czolgis1225";
 $database = "espweb";
 ?>
 
-<?php
-require_once 'connect.php';
-$conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-$q = "SELECT * FROM sensors";
-$result = $conn->query($q);
-
-$conn->close();
-?>
--- index.php
+++ index.php
@@ -2,23 +2,58 @@
 <html lang="pl">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
+    <meta name="description" content="ESP Weather Sensor Dashboard">
+    <meta name="author" content="Konisz">
     <title>ESP - WSI</title>
-
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
 </head>
+
+
+<?php
+require_once 'connect.php';
+$conn = new mysqli($servername, $username, $password, $database);
+$data1 ='';
+$data2 ='';
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    $db_status_holder = $conn->connect_error;
+}
+$db_status_holder = "Połączono";
+
+$last_reading_qry = "SELECT temperature,humidity,pressure,ts FROM sensors ORDER BY id DESC LIMIT 1";
+$last_reading_data = $conn->query($last_reading_qry);
+
+
+$last10_temperatures_qry = "SELECT temperature,ts FROM sensors ORDER BY id DESC LIMIT 200 ";
+$last10_temperatures_data = $conn->query($last10_temperatures_qry);
+
+while($row1 =mysqli_fetch_array($last10_temperatures_data))
+{
+    $data1 = $data1.'"'.$row1['ts'].'",';
+    $data2 = $data2.'"'.$row1['temperature'].'",';
+}
+$data1 = trim($data1,",");
+$data2 = trim($data2,",");
+
+
+foreach($last_reading_data as $row)
+{
+    $lasttime_holder = $row['ts'];
+    $temperature_holder = $row['temperature'];
+    $humidity_holder = $row['humidity'];
+    $pressure_holder = $row['pressure'];
+}
+
+$conn->close();
+?>
+
 
 <body id="page-top">
 
@@ -43,7 +78,8 @@
             <li class="nav-item active">
                 <a class="nav-link" href="index.html">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
+                    <span>Dashboard</span>
+                </a>
             </li>
 
             <!-- Divider -->
@@ -79,9 +115,6 @@
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Grafy</span></a>
             </li>
-
-
-
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
@@ -142,6 +175,7 @@
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-dark shadow-sm"><i class="fas fa-clock fa-sm text-white-50"></i><?php echo " Ostatni odczyt: ".$lasttime_holder ?></a>
                     </div>
 
                     <!-- Content Row -->
@@ -155,7 +189,7 @@
                                         <div class="col mr-2">
                                             <div class="text-sm font-weight-bold text-danger text-uppercase mb-1">
                                                 TEMPERATURA</div>
-                                            <div class="h1 mb-0 font-weight-bold text-gray-800">25.5°C</div>
+                                            <div class="h1 mb-0 font-weight-bold text-gray-800"><?php echo $temperature_holder."°C" ?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-temperature-high fa-4x text-gray-300"></i>
@@ -173,7 +207,7 @@
                                         <div class="col mr-2">
                                             <div class="text-sm font-weight-bold text-primary text-uppercase mb-1">
                                                 WILGOTNOŚĆ</div>
-                                            <div class="h1 mb-0 font-weight-bold text-gray-800">64.6 %</div>
+                                            <div class="h1 mb-0 font-weight-bold text-gray-800"><?php echo $humidity_holder." %"?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-tint fa-4x text-gray-300"></i>
@@ -191,7 +225,7 @@
                                         <div class="col mr-2">
                                             <div class="text-sm font-weight-bold text-success text-uppercase mb-1">
                                                 CIŚNIENIE</div>
-                                            <div class="h1 mb-0 font-weight-bold text-gray-800">1104 hPa</div>
+                                            <div class="h1 mb-0 font-weight-bold text-gray-800"><?php echo $pressure_holder." hPa"?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-sort-amount-up-alt fa-4x text-gray-300"></i>
@@ -208,8 +242,8 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-sm font-weight-bold text-info text-uppercase mb-1">
-                                                Status systemu</div>
-                                            <div class="h1 mb-0 font-weight-bold text-gray-800">Brak błędów</div>
+                                                Status bazy danych</div>
+                                            <div class="h1 mb-0 font-weight-bold text-gray-800"><?php echo $db_status_holder ?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-link fa-4x text-gray-300"></i>
@@ -226,11 +260,11 @@
 
                         <!-- Area Chart -->
                         <div class="col-xl-12">
-                            <div class="card shadow mb-4">
+                            <div class="card shadow mb-6">
                                 <!-- Card Header - Dropdown -->
                                 <div
                                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-dark">Odczyt czujników</h6>
+                                    <h6 class="m-0 font-weight-bold text-dark">Odczyt temperatury</h6>
                                 </div>
                                 <!-- Card Body -->
                                 <div class="card-body">
@@ -245,6 +279,8 @@
             </div>
         </div>
     </div>
+
+
                 <!-- /.container-fluid -->
         <!-- End of Content Wrapper -->
     <!-- End of Page Wrapper -->
@@ -262,7 +298,101 @@
     <!-- Page level plugins -->
     <script src="vendor/chart.js/Chart.min.js"></script>
     <!-- Page level custom scripts -->
-    <script src="js/demo/chart-area-demo.js"></script>
+<script>
+    // Set new default font family and font color to mimic Bootstrap's default styling
+    Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+    Chart.defaults.global.defaultFontColor = '#858796';
+    // Area Chart Example
+    var ctx = document.getElementById("myAreaChart");
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [<?php echo $data1 ?>],
+            datasets: [{
+                label: "Temperatura",
+                lineTension: 0.3,
+                backgroundColor: "rgba(78, 115, 223, 0.05)",
+                borderColor: "rgba(231,74,59, 1)",
+                pointRadius: 0,
+                pointBackgroundColor: "rgba(231,74,59, 1)",
+                pointBorderColor: "rgba(231,74,59, 1)",
+                pointHoverRadius: 3,
+                pointHoverBackgroundColor: "rgba(231,74,59, 1)",
+                pointHoverBorderColor: "rgba(231,74,59, 1)",
+                pointHitRadius: 1,
+                pointBorderWidth: 1,
+                data: [<?php echo $data2 ?>],
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            layout: {
+                padding: {
+                    left: 10,
+                    right: 25,
+                    top: 25,
+                    bottom: 0
+                }
+            },
+            scales: {
+                xAxes: [{
+                    gridLines: {
+                        color: "rgb(220,220,220)",
+                        drawBorder: false
+                    },
+                    ticks: {
+                        maxTicksLimit: 20
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        maxTicksLimit: 10,
+                        padding: 10,
+                        // Include a dollar sign in the ticks
+                        callback: function(value, index, values) {
+                            return value + "°C";
+                        }
+                    },
+                    gridLines: {
+                        color: "rgb(234, 236, 244)",
+                        zeroLineColor: "rgb(234, 236, 244)",
+                        drawBorder: false,
+                        borderDash: [2],
+                        zeroLineBorderDash: [2]
+                    }
+                }],
+            },
+            legend: {
+                display: false
+            },
+            tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                titleMarginBottom: 10,
+                titleFontColor: '#6e707e',
+                titleFontSize: 14,
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                intersect: false,
+                mode: 'index',
+                caretPadding: 10,
+                callbacks: {
+                    label: function(tooltipItem, chart) {
+                        var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                        return datasetLabel + ': $' + tooltipItem.yLabel;
+                    }
+                }
+            }
+        }
+    });
+
+
+
+</script>
+
 
 </body>
 
